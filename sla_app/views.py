92567e27b80ca35738f8d40d86b33f58cef74563
leoreq <leoreq@gmail.com>
2015-11-30 22:50:51 -0400

    trying to update the ModelForm, still unsuccesfully

--- sla_app/views.py
+++ sla_app/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from django.template import RequestContext, loader
+from sla_app.forms import CompanyForm
+
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+
+import warnings
+
+from django.conf import settings
+# Avoid shadowing the login() and logout() views below.
+from django.contrib.auth import (
+    REDIRECT_FIELD_NAME, get_user_model, login as auth_login,
+    logout as auth_logout, update_session_auth_hash,
+)
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.response import TemplateResponse
 
 # Create your views here.
 
@@ -16,15 +33,60 @@ def home(request):
     })
     return HttpResponse(template.render(context))
 
-def profile_update(request):
-    template = loader.get_template('sla_app/profile_update.html')
-    context = RequestContext(request, {
-        'test': 'test',
-    })
-    return HttpResponse(template.render(context))
+#def profile_update(request):
+#    template = loader.get_template('sla_app/profile_update.html')
+#    context = RequestContext(request, {
+#        'test': 'test',
+#    })
+#    return HttpResponse(template.render(context))
 
 # Create your views here.
+@sensitive_post_parameters()
+@csrf_protect
+@never_cache
+def profile_update(request, template_name='sla_app/profile_update.html',
+          redirect_field_name=REDIRECT_FIELD_NAME,
+          company_form=CompanyForm,
+          current_app=None, extra_context=None):
+    """
+    Displays the profile update form and handles the update action.
+    """
+    redirect_to = request.POST.get(redirect_field_name,
+                                   request.GET.get(redirect_field_name, ''))
 
+    if request.method == "POST":
+        form = company_form(request, data=request.POST)
+        if form.is_valid():
+
+            # Ensure the user-originating redirection url is safe.
+            if not is_safe_url(url=redirect_to, host=request.get_host()):
+                redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
+
+            # Create a form instance from POST data.
+            f = company_form(request.POST)
+
+            # Save a new Article object from the form's data.
+            update_profile = f.save()
+
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = company_form(request)
+
+    current_site = get_current_site(request)
+
+    context = {
+        'form': form,
+        redirect_field_name: redirect_to,
+        'site': current_site,
+        'site_name': current_site.name,
+    }
+    if extra_context is not None:
+        context.update(extra_context)
+
+    if current_app is not None:
+        request.current_app = current_app
+
+    return TemplateResponse(request, template_name, context)
 
 #def index(request):
 #    return HttpResponse("This will hold the SLAPP Landing Page")
