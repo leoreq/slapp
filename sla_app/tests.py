619d631f8bee7d6b0247f7e7b15c799dd9a84cd5
leoreq <leoreq@gmail.com>
2015-12-04 00:22:40 -0400

    passed model test unittest

--- sla_app/tests.py
+++ sla_app/tests.py
@@ -5,6 +5,7 @@ from sla_app.views import home,pag_inicio,profile_update
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from sla_app.models import Company
+from django.contrib.auth.models import User
 
 # Create your tests here.
 
@@ -38,23 +39,29 @@ class CompanyProfileUpdateTest(TestCase):
 class CompanyModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        first_user=User.objects.create_user('testuser1','test@gmail.com',password='testpass')
+        first_user.save()
+
         first_company = Company()
-        first_company.user=1
+        first_company.user=first_user
         first_company.name = 'New Testing Corp'
         first_company.service='This is a company that tests things'
         first_company.save()
 
+        second_user=User.objects.create_user('testuser2','test2@gmail.com',password='testpass2')
+        second_user.save()
+
         second_company = Company()
-        second_company.user=2
+        second_company.user=second_user
         second_company.name = 'Old Testing Corp'
         second_company.service='This is a company that should tests things'
         second_company.save()
 
-        saved_items = Item.objects.all()
+        saved_items = Company.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
+        first_saved_item = saved_items[1]
+        second_saved_item = saved_items[0]
         self.assertEqual(first_saved_item.name, 'New Testing Corp')
         self.assertEqual(second_saved_item.service, 'This is a company that should tests things')
 
