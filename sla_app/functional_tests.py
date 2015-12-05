c4435abd5e74f13b46730f2b3a01cb4ea8694f59
leoreq <leoreq@gmail.com>
2015-12-04 21:22:53 -0400

    Passing functional test for user creation, but needs to be able to use a test database for functional tests' '

--- sla_app/functional_tests.py
+++ sla_app/functional_tests.py
@@ -23,6 +23,14 @@ class NewCompanyTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def fill_element_id(self,element_id,placeholder,data_input):
+        inputbox = self.browser.find_element_by_id(element_id)
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                placeholder
+        )
+        inputbox.send_keys(data_input)
+
     def test_new_company_login(self):
 
         #Amibentes Proyectados company has heard about an app that will help them rate their provider work.
@@ -77,38 +85,57 @@ class NewCompanyTest(unittest.TestCase):
         #Juanito enters the sign up page.
         
         self.browser.find_element(By.PARTIAL_LINK_TEXT,"Sign Up Here").click();
+
         login_form=self.browser.find_element_by_id('loginform')
 
         #There he enters his login name.
-        inputbox = self.browser.find_element_by_id('login-username')
-        self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'username or email'
-        )
-        inputbox.send_keys('testuser')
-        inputbox.send_keys(Keys.ENTER)
-        
-        #import time
-        #time.sleep(10)
+        self.fill_element_id('login-username','username or email','testuser2')
+        self.fill_element_id('login-password','password','testpasss')
+        self.fill_element_id('login-password2','confirm password','testpasss')
+        self.browser.find_element_by_id('registerbutton').click();
+        import time
+        time.sleep(10)
+        #inputbox = self.browser.find_element_by_id('login-username')
+        #self.assertEqual(
+        #        inputbox.get_attribute('placeholder'),
+        #        'username or email'
+        #)
+        #inputbox.send_keys('testuser')
 
         #Juanito mistakedly entered mismatching paswwords. 
-        pass1inputbox = self.browser.find_element_by_id('login-password')
-        self.assertEqual(
-                pass1inputbox.get_attribute('placeholder'),
-                'password'
-        )
-        pass1inputbox.send_keys('testpasss')
-        pass1inputbox.send_keys(Keys.ENTER)
-        pass2inputbox = self.browser.find_element_by_id('login-password2')
-        self.assertEqual(
-                pass2inputbox.get_attribute('placeholder'),
-                'confirm password'
-        )
-        pass2inputbox.send_keys('mismatchingtestpasss')
-        pass2inputbox.send_keys(Keys.ENTER)
+        # Penging ...
         #Juanito then focuses and puts in matching passwords.
+        #inputbox = self.browser.find_element_by_id('login-password')
+        #self.assertEqual(
+        #        inputbox.get_attribute('placeholder'),
+        #        'password'
+        #)
+        #inputbox.send_keys('testpasss')
+        #inputbox = self.browser.find_element_by_id('login-password2')
+        #self.assertEqual(
+        #        inputbox.get_attribute('placeholder'),
+        #        'confirm password'
+        #)
+        #inputbox.send_keys('testpasss')
+        
+        
+        inputbox.send_keys(Keys.ENTER)
+        
 
         #Juanito gets prompted to a new page that asks for more info
+
+        
+        login_form=self.browser.find_element_by_id('profileupdateform')
+        inputbox = self.browser.find_element_by_id('id_company_name')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter Company Name'
+        )
+        pass2inputbox.send_keys('New Testing Corp')
+        pass2inputbox.send_keys(Keys.ENTER)
+
+
+        #
         self.fail('Finish the test')
 
     def check_item_in_row(self,item_name):
@@ -116,77 +143,6 @@ class NewCompanyTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(item_name,[row.text for row in rows])
 
-class ItemListTest(unittest.TestCase):
-    def setUp(self):
-        self.browser=webdriver.Chrome('/Users/LeeX/Dropbox/Programming/Thinkfull/python/django/slapp/sla_app/chromedriver')  # Optional argument, if not specified will search path.
-
-    def tearDown(self):
-        self.browser.quit()
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
-        self.browser.get('http://localhost:8000/pag_inicio')
-
-        # She notices the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
-        )
-
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list table
-        inputbox.send_keys(Keys.ENTER)
-
-
-
-        table = self.browser.find_element_by_id('id_list_table')
-
-        #table = self.browser.find_element(by=By.ID, value="id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_item_in_row('1: Buy peacock feathers')
-    
-
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very
-        # methodical)
-
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-                inputbox.get_attribute('placeholder'),
-                'Enter a to-do item'
-        )
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-
-
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-
-        #table = self.browser.find_element(by=By.ID, value="id_list_table")
-        rows = table.find_elements_by_tag_name('tr')
-        self.check_item_in_row('1: Buy peacock feathers')
-        self.check_item_in_row('2: Use peacock feathers to make a fly')
-        #import time
-        #time.sleep(2)
-        
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-
-        # She visits that URL - her to-do list is still there.
-
-        # Satisfied, she goes back to sleep
-        self.fail('Finish the test!')
 
 
 if __name__=='__main__':
